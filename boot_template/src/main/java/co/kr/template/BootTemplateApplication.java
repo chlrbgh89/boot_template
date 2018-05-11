46a2401e5d569021f7e35decd671ab8b39ea401f
CHOI KYU HO <chlrbgh89@naver.com>
2018-05-11 12:56:45 +0900

    set

---
+++ boot_template/src/main/java/co/kr/template/BootTemplateApplication.java
@@ -0,0 +1,12 @@
+package co.kr.template;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootTemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootTemplateApplication.class, args);
+	}
+}
